713e5ca02255a1fe5448f2e64f460cb070c6aacb
Alexander <friberg.alx@gmail.com>
2024-03-31 21:02:52 +0200

    prompt history

--- src/lib/components/Prompt.svelte
+++ src/lib/components/Prompt.svelte
@@ -9,17 +9,23 @@
 
 	function handleKeyDown(event: KeyboardEvent) {
 		if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+			if ($chat[$chat.length - 1 - cycler].author === 'AI') {
+				cycler += 1;
+			}
 			prompt = $chat[$chat.length - 1 - cycler].message;
 			cycler += 1;
 			if (cycler >= $chat.length) {
 				cycler = 0;
 			}
-			console.log($chat, cycler);
 		}
+	}
+
+	function resetCycler() {
+		cycler = 0;
 	}
 </script>
 
 <form on:submit|preventDefault={handleSubmit} class="flex h-full gap-4 p-4">
-	<TextInput bind:value={prompt} on:keydown={(e) => handleKeyDown(e)} />
+	<TextInput bind:value={prompt} on:input={resetCycler} on:keydown={(e) => handleKeyDown(e)} />
 	<button class="rounded-lg bg-blue-500 px-6 hover:bg-blue-400">😎</button>
 </form>
